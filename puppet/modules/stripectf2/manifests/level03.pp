0566aa1740174ed930e5e50f99bcfdc86a4c3afc
Daniel Jackson <e28eta@yahoo.com>
2013-03-12 17:36:59 -0700

    Fix level 3.
    Need to create data directory, and then call generate_data.py with a randomly generated password.
    
    Also need a local_settings.py file that sets url_root to nothing (in real contest it was
    the user's personal directory)

--- puppet/modules/stripectf2/manifests/level03.pp
+++ puppet/modules/stripectf2/manifests/level03.pp
@@ -6,7 +6,7 @@ class stripectf2::level03 (
 		ensure => 'running',
 		start => "cd ${destination} && python secretvault.py &",
 		provider => 'base',
-		require => File[$destination]
+		require => Exec['generate_data.py'],
 	}
 
 	file {$destination:
@@ -22,5 +22,29 @@ class stripectf2::level03 (
 
 	stripectf2::apache2 {'level03':
 		proxy_url => "http://127.0.0.1:5000/"
+	}
+	
+	file {"${destination}/data":
+		ensure => 'directory',
+		require => File[$destination],
+	}
+	
+	stripectf2::random_password {"${destination}/data/password.txt":
+		require => File["${destination}/data"],
+	}
+	
+	exec {'generate_data.py':
+		creates => ["${destination}/data/users.db", "${destination}/data/secrets.json", "${destination}/data/entropy.dat"],
+		command => "${destination}/generate_data.py \"${destination}/data\" `cat ${destination}/data/password.txt` \"P = NP because I said so\" \"Perpetual motion is a myth\"",
+		require => Stripectf2::Random_password["${destination}/data/password.txt"],
+		subscribe => Stripectf2::Random_password["${destination}/data/password.txt"],
+		notify => Service['secretvault.py'],
+	}
+	
+	file {"${destination}/local_settings.py":
+		content => "url_root = ''",
+		mode => '0755',
+		owner=> 'vagrant',
+		group=> 'vagrant',
 	}
 }
