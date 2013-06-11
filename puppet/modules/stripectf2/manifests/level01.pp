35155969906ca81c0ba74f477e84254e488e50c6
Daniel Jackson <e28eta@yahoo.com>
2013-06-11 13:38:21 -0700

    ignore secret-combination.txt, create one with random contents, and fix a typo (?) on the password file
    Trying to use htaccess to block access

--- puppet/modules/stripectf2/manifests/level01.pp
+++ puppet/modules/stripectf2/manifests/level01.pp
@@ -11,18 +11,36 @@ class stripectf2::level01 (
 		owner => 'www-data',
 		group => 'www-data',
 		source => $source,
-		ignore => 'level02-password.txt', # Don't copy the dummy password.txt file
+		ignore => ['level02-password.txt', 'secret-combination.txt'], # Don't copy the dummy password or combo file
 	}
 	
 	stripectf2::random_password {"${destination}/level02-password.txt":
 		require => File[$destination],
 	}
 	
-	file {"${destination}/password.txt":
-		mode => '0333',
+	file {"${destination}/level02-password.txt":
+		mode => '0777',
 		owner => 'www-data',
 		group => 'www-data',
 		require => Stripectf2::Random_password["${destination}/level02-password.txt"],
+	}
+	
+	stripectf2::random_password {"${destination}/secret-combination.txt":
+		require => File[$destination],
+	}
+	
+	file {"${destination}/secret-combination.txt":
+		mode => '0777',
+		owner => 'www-data',
+		group => 'www-data',
+		require => Stripectf2::Random_password["${destination}/secret-combination.txt"],
+	}
+	
+	file {"${destination}/.htaccess":
+		content => "<FilesMatch \".(txt)\">
+ Order allow,deny
+ Deny from all
+ </FilesMatch>"
 	}
 	
 	stripectf2::apache2 {'level01':
